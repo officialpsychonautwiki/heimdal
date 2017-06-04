7074749012036b7d57e390a02fb3df4187a362cf
Kenan Sulayman <kenan@sly.mn>
2017-06-04 22:23:18 +0200

    Update base code to support heimdal better

--- heimdal.js
+++ heimdal.js
@@ -37,203 +37,217 @@ Countly.prototype.device_id;
 /** @type {Countly} */
 window.Countly;
 
-class Heimdal {
-	constructor () {
-		super();
+class Events {
+    constructor () {
+        this._events = {};
+    }
 
-		this._constants = {
-			endpoint: 'https://h.psychonautwiki.org/ingress',
+    on (evt, fn) {
+        this._events[evt] = fn;
+    }
 
-			grace: 200,
-			max: 15,
-			storeName: '__hd_d'
-		};
+    emit (evt, ...args) {
+        this._events[evt](...args);
+    }
+}
 
-		this._internal = {
-			_tt: 0,
-			_retries: 0
-		};
+class Heimdal extends Events {
+    constructor () {
+        super();
 
-		this._plugins = [];
-		this._eventHeap = [];
+        this._constants = {
+            endpoint: 'https://h.psychonautwiki.org/ingress',
 
-		this._clientId = this._getClientId();
-	}
+            grace: 200,
+            max: 15,
+            storeName: '__hd_d'
+        };
 
-	_getClientId () {
-		let clientId;
+        this._internal = {
+            _tt: 0,
+            _retries: 0
+        };
 
-		try {
-			clientId = window['Countly']['device_id'];
-		} catch (e) {}
+        this._plugins = [];
+        this._eventHeap = [];
 
-		if (!clientId) {
-			this._ingestError(new Error('Heimdal: Could not identify user'));
-		}
+        this._clientId = this._getClientId();
+    }
 
-		return clientId;
-	}
+    _getClientId () {
+        let clientId;
 
-	_ingestError (err) {
-		if ('Raven' in window && 'captureException' in Raven) {
-			/* send to sentry */
-			Raven.captureException(err);
-		}
-	}
+        try {
+            clientId = window['Countly']['device_id'];
+        } catch (e) {}
 
-	_ingestEvent (evt) {
-		this._addEvent(evt);
-		this._queueTelemetry();
-	}
+        if (!clientId) {
+            this._ingestError(new Error('Heimdal: Could not identify user'));
+        }
 
-	_addEvent (evt) {
-		let user = null;
+        return clientId;
+    }
 
-		try {
-			user = window['mw']['config']['values']['wgUserName'];
-		} catch(err) { this._ingestError(err) }
+    _ingestError (err) {
+        if ('Raven' in window && 'captureException' in Raven) {
+            /* send to sentry */
+            Raven['captureException'](err);
+        }
+    }
 
-		this._eventHeap.push(`${this._clientId};${Date.now()};${user};${evt}`);
-		this._persistEventHeap();
-	}
+    _ingestEvent (evt) {
+        this._addEvent(evt);
+        this._queueTelemetry();
+    }
 
-	_storageStrategy () {
-		const ts = "301";
+    _addEvent (evt) {
+        let user = null;
 
-		try {
-			window.sessionStorage._pcl = ts;
+        try {
+            user = window['mw']['config']['values']['wgUserName'];
+        } catch(err) { this._ingestError(err) }
 
-			if (ts === window.sessionStorage._pcl) {
-				window.sessionStorage.removeItem('_pcl');
+        this._eventHeap.push(`${this._clientId};${Date.now()};${user};${evt}`);
+        this._persistEventHeap();
+    }
 
-				return 1;
-			}
-		} catch(e) {}
+    _storageStrategy () {
+        const ts = "301";
 
-		try {
-			window.localStorage._pcl = ts;
+        try {
+            window['sessionStorage']['_pcl'] = ts;
 
-			if (ts === window.localStorage._pcl) {
-				window.localStorage.removeItem('_pcl');
+            if (ts === window['sessionStorage']['_pcl']) {
+                window['sessionStorage']['removeItem']('_pcl');
 
-				return 2;
-			}
-		} catch(e) {}
-	}
+                return 1;
+            }
+        } catch(e) {}
 
-	_obtainEventHeap () {
-		let data;
+        try {
+            window['localStorage']['_pcl'] = ts;
 
-		switch(this._storageStrategy()) {
-			case 1:
-				data = window.sessionStorage[this._constants.storeName];
-				break;
-			case 2:
-				data = window.localStorage[this._constants.storeName];
-				break;
-		}
+            if (ts === window['localStorage']['_pcl']) {
+                window['localStorage']['removeItem']('_pcl');
 
-		try {
-			this._eventHeap = JSON.parse(data);
-		} catch(e) {}
-	}
+                return 2;
+            }
+        } catch(e) {}
+    }
 
-	_persistEventHeap () {
-		const dump = JSON.stringify(this._eventHeap);
+    _obtainEventHeap () {
+        let data;
 
-		switch(this._storageStrategy()) {
-			case 1:
-				window.sessionStorage[this._constants.storeName] = dump;
-				return;
-			case 2:
-				window.localStorage[this._constants.storeName] = dump;
-				return;
-			default:
-				this._ingestError(new Error('Heimdal: Could not identify storage strategy'));
-		}
-	}
+        switch(this._storageStrategy()) {
+            case 1:
+                data = window['sessionStorage'][this._constants.storeName];
+                break;
+            case 2:
+                data = window['localStorage'][this._constants.storeName];
+                break;
+        }
 
-	_resetEventHeap () {
-		this._eventHeap = [];
-		this._persistEventHeap();
-	}
+        try {
+            this._eventHeap = window['JSON']['parse'](data);
+        } catch(e) {}
+    }
 
-	_queueTelemetry () {
-		clearTimeout(this._internal._tt);
+    _persistEventHeap () {
+        const dump = window['JSON']['stringify'](this._eventHeap);
 
-		this._internal._tt = setTimeout(
-			this._transferTelemetry.bind(this),
-			(100 + (this._internal._retries * this._constants.grace))
-		);
-	}
+        switch(this._storageStrategy()) {
+            case 1:
+                window['sessionStorage'][this._constants.storeName] = dump;
+                return;
+            case 2:
+                window['localStorage'][this._constants.storeName] = dump;
+                return;
+            default:
+                this._ingestError(new Error('Heimdal: Could not identify storage strategy'));
+        }
+    }
 
-	_requeueTelemetry () {
-		this._internal._retries++;
-		this._queueTelemetry();
-	}
+    _resetEventHeap () {
+        this._eventHeap = [];
+        this._persistEventHeap();
+    }
 
-	_transferTelemetry () {
-		try {
-			const xhr = new XMLHttpRequest();
+    _queueTelemetry () {
+        clearTimeout(this._internal._tt);
 
-			xhr.open("POST", this._constants.endpoint, true);
+        this._internal._tt = setTimeout(
+            this._transferTelemetry.bind(this),
+            (100 + (this._internal._retries * this._constants.grace))
+        );
+    }
 
-			xhr.setRequestHeader('Content-Type', 'application/json');
+    _requeueTelemetry () {
+        this._internal._retries++;
+        this._queueTelemetry();
+    }
 
-			xhr.onreadystatechange = () => {
-				if (xhr.readyState == 4) {
-					if (xhr.status !== 200) {
-						return this._requeueTelemetry();
-					}
+    _transferTelemetry () {
+        try {
+            const xhr = new window['XMLHttpRequest']();
 
-					this._resetEventHeap();
-					this._internal._retries = 0;
-				}
-			};
+            xhr['open']("POST", this._constants.endpoint, true);
 
-			xhr.onerror = this._requeueTelemetry.bind(this);
+            xhr['setRequestHeader']('Content-Type', 'application/json');
 
-			xhr.send(JSON.stringify(this._eventHeap));
-		} catch(e) {
-			this._ingestError(e);
-		}
-	}
+            xhr['onreadystatechange'] = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status !== 200) {
+                        return this._requeueTelemetry();
+                    }
 
-	use (ref, plugin) {
-		this._plugins.push([ref, plugin]);
-	}
+                    this._resetEventHeap();
+                    this._internal._retries = 0;
+                }
+            };
 
-	init () {
-		this._obtainEventHeap();
+            xhr['onerror'] = this._requeueTelemetry.bind(this);
 
-		if (this._eventHeap.length) {
-			this._queueTelemetry();
-		}
+            xhr['send'](window['JSON']['stringify'](this._eventHeap));
+        } catch(e) {
+            this._ingestError(e);
+        }
+    }
 
-		for (let i = 0; i < this._plugins.length; ++i) {
-			try {
-				this._plugins[i][1](this);
-			} catch(err) {
-				this._ingestError(new Error(`Heimdal: '${this._plugins[i][0]}' failed: ${err}`));
-			}
-		}
-	}
+    use (ref, plugin) {
+        this._plugins.push([ref, plugin]);
+    }
+
+    init () {
+        this._obtainEventHeap();
+
+        if (this._eventHeap.length) {
+            this._queueTelemetry();
+        }
+
+        for (let i = 0; i < this._plugins.length; ++i) {
+            try {
+                this._plugins[i][1](this);
+            } catch(err) {
+                this._ingestError(new Error(`Heimdal: '${this._plugins[i][0]}' failed: ${err}`));
+            }
+        }
+    }
 }
 
 let heimdal = new Heimdal();
 
 heimdal.use('t', ctx => {
-	ctx._ingestEvent(`t;${window['mw']['config']['values']['wgPageName']}`);
+    ctx._ingestEvent(`t;${window['mw']['config']['values']['wgPageName']}`);
 });
 
 heimdal.use('a', ctx => {
-	$('a').click(evt => {
-		if (!evt.target || !evt.target.href) return;
+    $('a').click(evt => {
+        if (!evt['target'] || !evt['target']['href']) return;
 
-		if (!evt.target.href.match(/\#$/) && evt.target.match(/\/wiki\/(.*?)$/)) {
-			ctx._ingestEvent(`a;${window['mw']['config']['values']['wgPageName']};${evt.target.href.match(/\/wiki\/(.*?)$/)[1]}`)
-		}
-	})
+        if (!evt['target']['href']['match'](/\#$/) && evt['target']['href']['match'](/\/wiki\/(.*?)$/)) {
+            ctx._ingestEvent(`a;${window['mw']['config']['values']['wgPageName']};${evt['target']['href']['match'](/\/wiki\/(.*?)$/)[1]}`)
+        }
+    })
 });
 
 heimdal.init();
